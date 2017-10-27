33d45397bcb5ffe5d9f0f656c908b917ecbe27f7
= <jacksonwilson99@gmail.com>
2017-10-27 00:15:21 -0500

    Add sorts

--- CSCI_131_2017/src/main/java/utils/SortAlgorithms.java
+++ CSCI_131_2017/src/main/java/utils/SortAlgorithms.java
@@ -36,9 +36,13 @@ public class SortAlgorithms {
     }
 
     public static void insertion(int[] arr) {
-        for (int i = 1; i < arr.length - 1; i++) {
+        insertionRanged(arr, 0, arr.length);
+    }
+
+    private static void insertionRanged(int[] arr, int l, int r) {
+        for (int i = 1; i < r - 1; i++) {
             int j = i;
-            while (j > 0 && arr[j] > arr[j + 1]) {
+            while (j > l && arr[j] > arr[j + 1]) {
                 swap(arr, j, j + 1);
                 j--;
             }
@@ -245,6 +249,153 @@ public class SortAlgorithms {
             }
             swap(arr, i, indexOfMin);
         }
+    }
+
+    public static void tim(int[] arr) {
+        int run = 32;
+
+        for (int i = 0; i < arr.length; i += run) {
+            insertionRanged(arr, i, Math.min(i + 31, arr.length - 1));
+        }
+
+        for (int size = run; size < arr.length; size *= 2) {
+            for (int left = 0; left < arr.length; left += 2 * size) {
+                int mid = left + size - 1;
+                int right = Math.min(left + 2 * size - 1, arr.length - 1);
+                mergeIterative(arr, left, mid, right);
+            }
+        }
+    }
+
+    private static void mergeIterative(int[] arr, int l, int m, int r) {
+        int left[] = new int[m - l + 1];
+        int right[] = new int[r - m];
+
+        for (int i = 0; i < left.length; i++)
+            left[i] = arr[l + i];
+        for (int i = 0; i < right.length; i++)
+            right[i] = arr[m + 1 + i];
+     
+        int i = 0;
+        int j = 0;
+        int k = l;
+     
+        for (; i < left.length && j < right.length; k++) {
+            if (left[i] <= right[j])
+                arr[k] = left[i++];
+            else
+                arr[k] = right[j++];
+        }
+
+        for (; i < left.length; k++, i++)
+            arr[k] = left[i];
+     
+        for (; i < right.length; k++, i++)
+            arr[k] = right[i];
+    }
+
+    public static void gnome(int[] arr) {
+        for (int i = 0; i < arr.length; ) {
+            if (i == 0)
+                i++;
+            
+            if (arr[i] >= arr[i - 1]) {
+                i++;
+            } else {
+                swap(arr, i, i - 1);
+                i--;
+            }
+        }
+    }
+
+    public static void counting(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return;
+        
+        int[] minMaxValues = minMaxValues(arr);
+
+        counting(arr, minMaxValues[0], minMaxValues[1]);
+    }
+
+    private static void counting(int[] arr, int min, int max) {
+        int[] buckets = new int[max - min + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            buckets[arr[i] - min]++;
+        }
+
+        for (int i = 0, sortedIndex = 0; i < buckets.length; i++) {
+            while (buckets[i] > 0) {
+                arr[sortedIndex++] = i + min;
+                buckets[i]--;
+            }
+        }
+    }
+
+    public static void radix(int[] arr) {
+        for (int e = 1; maxValue(arr) / e > 0; e *= 10) {
+            counting(arr, arr.length, e);
+        }
+    }
+
+    public static void shell(int[] arr) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                    arr[j] = arr[j - gap];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    public static void pigeonhole(int[] arr) {
+        int[] minMaxValues = minMaxValues(arr);
+        int min = minMaxValues[0];
+        int max = minMaxValues[1];
+        int range = max - min + 1;
+        int[] pigeonhole = new int[range];
+        Arrays.fill(pigeonhole, 0);
+
+        for (int i = 0; i < arr.length; i++)
+            pigeonhole[arr[i] - min]++;
+
+        for (int j = 0, index = 0; j < range; j++) {
+            for (; pigeonhole[j] > 0; pigeonhole[j]--, index++)
+                arr[index] = j + min;
+        }
+    }
+
+    private static int minValue(int[] arr) {
+        int min = arr[0];
+        for (int n : arr) {
+            if (n > min)
+                min = n;
+        }
+        return min;
+    }
+
+    private static int maxValue(int[] arr) {
+        int max = arr[0];
+        for (int n : arr) {
+            if (n > max)
+                max = n;
+        }
+        return max;
+    }
+
+    private static int[] minMaxValues(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int n : arr) {
+            if (n > max)
+                max = n;
+            else if (n < min)
+                min = n;
+        }
+        return new int[]{min, max};
     }
 
     private static <T extends Comparable<T>> void swap(ArrayList<T> arr, int i, int j) {
